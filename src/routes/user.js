2ec461dd689e57a4674259d1bf90457b9c6d8a88
Santiago Taco <santiago.taco13@gmail.com>
2022-09-11 12:42:59 -0500

    refactor: remove unused code

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,5 @@
 import express from 'express';
-// controllers
-//import user from '../controllers/user.js';
 
 const router = express.Router();
-
-router
-  //.get('/', user.onGetAllUsers)
-  .post('/sign_up', user.onCreateUser)
-  //.get('/:id', user.onGetUserById)
-  //.delete('/:id', user.onDeleteUserById)
 
 export default router;
